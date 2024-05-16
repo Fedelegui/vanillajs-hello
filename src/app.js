1d32b37d015b27c13bd63e4d34627c869420d8b2
Fedelegui <107227090+Fedelegui@users.noreply.github.com>
2024-05-16 02:53:55 +0000

    Initial commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,31 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const suits = ["heart", "spade", "club", "diamond"];
+  const values = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King",
+    "Ace"
+  ];
+
+  function generateRandomCard() {
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+    const cardElement = document.getElementById("card");
+    cardElement.innerHTML = randomValue;
+    cardElement.classList.remove("heart", "spade", "club", "diamond");
+    cardElement.classList.add(randomSuit);
+  }
+
+  generateRandomCard();
 };
